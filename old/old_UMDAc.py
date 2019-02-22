312e56499895e60ff448175a9e5de31cbcedae34
mikel <mikelma7@gmail.com>
2019-02-22 17:56:43 +0100

    Old versions moved to old/

---
+++ old/old_UMDAc.py
@@ -0,0 +1,285 @@
+#!/usr/bin/env python3
+
+# Univariate Marginal Distribution Algorithm
+
+import numpy as np
+import matplotlib.pyplot as plt
+import random
+import copy
+import time
+
+from keras.models import model_from_json
+import keras
+
+class UMDAc():
+
+    def __init__(self, 
+                 model, 
+                 problem, 
+                 gen_size): 
+
+        np.random.seed(None) 
+
+        ## Global variables
+        self.model = model
+        self.gen_size = gen_size
+        
+        self.problem = problem
+
+        if self.model != None:
+
+            self.fitness = {} # Init fitness log
+            
+            ## Create first generation randomly
+            self.gen = {} # Init generation 0
+
+            ## Create random specimens
+            for i in range(gen_size):
+                ## Generate specimen weights and biases
+                specimen = []
+                for layer in model.get_weights():
+                    specimen.append(np.random.uniform(
+                        -1,1,layer.shape))                            
+                self.gen['s'+str(i)] = np.array(specimen)
+
+        self.fitness = {}
+        
+        ## Initialize training fitness logger
+        self.history = {
+            'avg':[],
+            'min':[],
+            'max':[]}
+
+    def train(self, surv, rand_surv, 
+              selection_mode='max', noise=None):
+
+        n_surv = int(self.gen_size*surv)
+        n_random_surv = int(n_surv*rand_surv)
+        n_surv -= n_random_surv
+
+        survivors = []
+        survivors_fitness = []
+
+        for name in self.gen:
+            
+            survivors.append(name)
+
+            specimen = self.gen[name]
+            t_reward = self.problem.evaluate(specimen, 
+                                             self.model) 
+            self.fitness[name] = t_reward
+
+        ## Collect data about generation
+        survivors = list(self.fitness.keys()) ## Survivors' names
+        survivors_fitness = list(self.fitness.values()) ## Survivors's fitnesses
+
+        self.history['avg'].append(np.mean(survivors_fitness))
+        self.history['min'].append(min(survivors_fitness))
+        self.history['max'].append(max(survivors_fitness))
+
+        worsts = [] ## Worst specimens names
+        worsts_fitness = [] ## Worst specimens fitness values
+
+        ## Select best fitness survivors
+        n_r = len(survivors) - n_surv ## Number of not survivor specimens 
+        for n in range(n_r):
+            
+            ## Select worst specimen
+            if selection_mode == 'max':
+                indx = survivors_fitness.index(min(survivors_fitness))
+
+            elif selection_mode == 'min':
+                indx = survivors_fitness.index(max(survivors_fitness))
+
+            ## Save worsts 
+            worsts.append(survivors[indx])    
+            worsts_fitness.append(survivors_fitness[indx])
+            ## Delete worsts from survivors lists
+            del survivors[indx]
+            del survivors_fitness[indx]
+
+        ## Randomly select bad specimens to survive
+        for i in range(n_random_surv):
+            ## Random index
+            indx = np.random.randint(len(worsts))
+            ## Add random specimen to survivors 
+            survivors.append(worsts[indx])
+            survivors_fitness.append(worsts_fitness[indx])
+            ## Update worst specimens' lists
+            del worsts[indx]
+            del worsts_fitness[indx]
+        
+        ## Generate new specimens (empty):
+        self.new = {}
+        for i in range(len(worsts)):
+            self.new['n'+str(i)] = copy.deepcopy(self.gen['s0'])
+
+        for i_l, layer in enumerate(self.gen['s0']):
+
+            if len(layer.shape) == 1: 
+                layer = layer.reshape((1, layer.shape[0]))
+                isbias = True
+            else:
+                isbias = False
+
+            for i in range(layer.shape[0]):
+                for j in range(layer.shape[1]):
+                    ## layer[i][j] weight of each survivor 
+                    w = []
+                    ## For each survivor
+                    for name in survivors:
+                        if isbias:
+                            w.append(self.gen[name][i_l][j])
+                        else:
+                            w.append(self.gen[name][i_l][i][j])
+
+                    if noise != None:
+                        n_mut = int(len(w)*noise)
+                        muts = np.random.rand(n_mut)
+
+                        w = np.array(w)
+                        np.random.shuffle(w)
+                        
+                        w = np.delete(w, range(len(w)-n_mut, len(w)), 0)
+
+                        w = np.hstack((w, muts))
+                        np.random.shuffle(w)
+                    
+                    ## Compute weights list's mean 
+                    mean = np.mean(w)
+                    ## Standard deviation
+                    std = np.std(w)
+                    
+                    ## Get samples
+                    samples = np.random.normal(mean, std, 
+                                               len(worsts))
+                    
+                    i_sample = 0 ##  Iterator
+                    ## Generate new specimens
+                    for name in self.new:
+                        ## Update weight  
+                        if isbias:
+                            self.new[name][
+                                i_l][j] = samples[i_sample]
+                        else:
+                            self.new[name][
+                                i_l][i][j] = samples[i_sample]
+                        i_sample += 1 
+        
+        ## After generating a set of new specimens
+        new_names = []
+        new_fitness = [] 
+
+        for name in self.new:
+            ## Evaluate new specimens
+            ## and store data for later comparison
+            new_names.append(name)
+            specimen = self.new[name]
+            ## Load
+            new_fitness.append(self.problem.evaluate(
+                specimen, self.model))
+
+        '''
+        Selection. Replace all specimens in the worsts list
+        with best specimens of the to_select lists.
+        '''
+        to_select_names = new_names+worsts
+        to_select_fitness = new_fitness+worsts_fitness
+
+        for i in range(len(worsts)):
+            ## Select best
+            if selection_mode == 'max':
+                indx = np.argmax(to_select_fitness)
+            elif selection_mode == 'min':
+                indx = np.argmin(to_select_fitness)
+            
+            ## Add selected specimen to new generation
+            if 'n' in to_select_names[indx]:
+                ## Replace specimen
+                self.gen[worsts[i]] = copy.deepcopy(self.new[
+                to_select_names[indx]])
+
+            else:
+                ## Replace specimen
+                self.gen[worsts[i]] = copy.deepcopy(self.gen[
+                to_select_names[indx]])
+
+            ## Update selection lists
+            del to_select_names[indx]
+            del to_select_fitness[indx]
+
+        return self.history
+
+    def save_specimen(self, specimen, filename='specimen.h5'):
+
+        from keras.models import load_model
+        ## Load specimen's weights to model        
+        self.model.set_weights(specimen)
+        ## Save model and specimen's weights
+        self.model.save(filename)  
+
+    def load_model(self, filename='specimen.h5'):
+
+        del self.model  # delete the existing model
+
+        from keras.models import load_model
+        ## Load new model and weights 
+        self.model = load_model(filename)
+
+if __name__ == '__main__':
+
+    import numpy as np
+
+    import keras
+
+    from keras.models import Model
+    from keras.layers import Input, Dense
+
+    from Wrappers.Gym import Gym
+
+    cartpole = Gym('CartPole-v0')
+
+    GENERATIONS = 15
+    GEN_SIZE = 30
+    SURV = .5
+    RAND_SURV = .3 
+
+    NOISE = None 
+
+    a = Input(shape=(4,))
+    b = Dense(2)(a)
+
+    model = Model(inputs=a, outputs=b)
+
+    umdac = UMDAc(model,
+                 problem=cartpole,
+                 gen_size=GEN_SIZE)
+
+    for generation in range(GENERATIONS):
+
+        history = umdac.train(surv=SURV, 
+                    rand_surv=RAND_SURV,
+                    noise=NOISE)
+
+        avg_f = history['avg'][-1]
+
+        print(generation, ' / ', GENERATIONS,' avg reward: ', avg_f)
+
+    ## Render best speciemens
+    print('')
+    print('-'*5, ' Rendering best specimen ', '-'*5)
+
+    ## Select best specimen 
+    names = list(umdac.fitness.keys())
+    f = list(umdac.fitness.values())
+
+    best = umdac.gen[names[f.index(max(f))]]
+
+    cartpole.iterations = 100
+
+    t_r = cartpole.evaluate(best, model, 
+                            render=True,
+                            verbose=True)
+
+    print('Total reward: ', t_r)
+
---
+++ old/old_lunar.py
@@ -0,0 +1,103 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+import keras
+
+from keras.models import Model
+from keras.layers import Input, Dense
+
+from UMDAc.UMDAc import UMDAc
+from UMDAc.Wrappers.Gym import Gym
+
+### HYPERPARAMETERS ###
+GENERATIONS = 500
+GEN_SIZE = 200
+SURV = .5
+RAND_SURV = .3 
+
+NOISE = None 
+FILENAME = 'lunar_result.h5' # Filename of best specimen
+# Problem specific
+MAX_STEPS = 400
+ITERATIONS = 3
+
+## Initialize Gym problem 
+problem = Gym('LunarLander-v2',
+              iterations=ITERATIONS,
+              max_steps=MAX_STEPS)
+
+## Initialize model
+a = Input(shape=(8,))
+b = Dense(4)(a)
+
+model = Model(inputs=a, outputs=b)
+
+## Initialize UMDAc
+umdac = UMDAc(model,
+             problem=problem,
+             gen_size=GEN_SIZE)
+
+model.summary()
+
+### TRAINING ###
+for generation in range(GENERATIONS):
+
+    ## Train
+    history = umdac.train(surv=SURV, 
+                rand_surv=RAND_SURV,
+                noise=NOISE)
+
+    ## Generation's average total reward
+    avg_f = history['avg'][-1]
+
+    print(generation, ' / ', GENERATIONS,' avg reward: ', avg_f)
+
+    ## Plot training info online
+    plt.clf()
+
+    plt.plot(range(generation + 1), history['min'],
+             label='Minimum')
+    plt.plot(range(generation + 1), history['max'],
+             label='Maximum')
+    plt.plot(range(generation + 1), history['avg'], 
+             label='Average')
+    plt.grid(b=True, which='major', color='#DDDDDD', 
+             linestyle='-')
+    plt.legend()
+    plt.xlabel('Generation')
+    plt.ylabel('Reward')
+
+    plt.draw()
+    plt.pause(.00001)
+    
+    ## Save best specimen to .h5 file 
+    if max(history['max']) == history['max'][-1]:
+
+        names = list(umdac.fitness.keys())
+        f = list(umdac.fitness.values())
+
+        best = umdac.gen[names[f.index(max(f))]]
+        umdac.save_specimen(best, FILENAME)
+        print('Best specimen saved')
+
+## Plot graph
+plt.show()
+
+## Render best speciemens
+print('')
+print('-'*5, ' Rendering best specimen ', '-'*5)
+
+## Select best specimen 
+names = list(umdac.fitness.keys())
+f = list(umdac.fitness.values())
+
+best = umdac.gen[names[f.index(max(f))]]
+
+problem.iterations = 100
+
+## Render best specimen
+t_r = problem.evaluate(best, model, 
+                        render=True,
+                        verbose=True)
+print('Average reward of 100 iterations: ', t_r)
+
